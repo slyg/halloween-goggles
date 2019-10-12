2b3931955fc2ce13df0f4159702b573e4dbb6978
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-10-12 15:57:21 +0100

    Rotating color gradient

--- rainbow/rainbow.ino
+++ rainbow/rainbow.ino
@@ -1,37 +1,11 @@
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
-#endif
 
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1:
 #define LED_PIN    6
-
-// How many NeoPixels are attached to the Arduino?
 #define LED_COUNT 16
 
-// Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-// Argument 1 = Number of pixels in NeoPixel strip
-// Argument 2 = Arduino pin number (most are valid)
-// Argument 3 = Pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-
-
-// setup() function -- runs once at startup --------------------------------
 
 void setup() {
-  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
-  // Any other board, you can remove this part (but no harm leaving it):
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
-  // END of Trinket-specific code.
-
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP
   strip.setBrightness(15); // Set BRIGHTNESS to about 1/5 (max = 255)
@@ -40,27 +14,25 @@ void setup() {
 
 void loop() {
 
-  long pixelHueStart = 0*65536;
-
-  long pixelHueEnd = 0.2*65536;
-
-  int interval = 256;
-
+  float hueInitialFactor = 0;
+  float hueIntervalFactor = 0.2;
+  long pixelHueStart = hueInitialFactor * 65536;
+  long pixelHueEnd = (hueInitialFactor + hueIntervalFactor) * 65536;
+  long hueInterval = hueIntervalFactor * 65536L / LED_COUNT;
   int delayBetweenUpdates = 10;
 
-  for(long pixelHue = pixelHueStart; pixelHue < pixelHueEnd; pixelHue += interval) {
-    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-      setPixelHue(i, pixelHue);
+  for(int shift = 0; shift < LED_COUNT; shift++) {
+
+    int modShift = shift % LED_COUNT;
+
+    for(int i=0; i < LED_COUNT; i++) {
+      int pixelHue = pixelHueStart + (i * hueInterval);
+      setPixelHue((i + modShift) % LED_COUNT, pixelHue);
+      strip.show();
     }
-    strip.show();
+
     delay(delayBetweenUpdates);
-  }
-  for(long pixelHue = pixelHueEnd; pixelHue > pixelHueStart; pixelHue -= interval) {
-    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-      setPixelHue(i, pixelHue);
-    }
-    strip.show();
-    delay(delayBetweenUpdates);
+
   }
 }
 
