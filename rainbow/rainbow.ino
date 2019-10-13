ab290208e91f9b3c094a8444013c655082661ce5
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-10-13 13:28:15 +0100

    Alternate randomly btw red and orange

--- rainbow/rainbow.ino
+++ rainbow/rainbow.ino
@@ -1,8 +1,10 @@
 #include <Adafruit_NeoPixel.h>
 
-#define LED_PIN_RIGHT   6
-#define LED_PIN_LEFT    7
-#define LED_COUNT      16
+#define LED_PIN_RIGHT      6
+#define LED_PIN_LEFT       7
+#define LED_COUNT         16  // 16 leds rings
+#define RINGS_BRIGHTNESS  20  // (0 to 255)
+#define HUE_RANGE         65536 // Number of possible hue values
 
 Adafruit_NeoPixel ringRight = Adafruit_NeoPixel(LED_COUNT, LED_PIN_RIGHT, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel ringLeft = Adafruit_NeoPixel(LED_COUNT, LED_PIN_LEFT, NEO_GRB + NEO_KHZ800);
@@ -13,38 +15,37 @@ void setup() {
 }
 
 void loop() {
-
-  float hueInitialFactor = 0;
-  float hueIntervalFactor = 0.2;
-  long pixelHueStart = hueInitialFactor * 65536;
-  long pixelHueEnd = (hueInitialFactor + hueIntervalFactor) * 65536;
-  long hueInterval = hueIntervalFactor * 65536L / LED_COUNT;
+  long pixelHueStart = 0; // Red
+  long pixelHueEnd = HUE_RANGE/6; // Orange
+  long hueInterval = 256;
   int delayBetweenUpdates = 10;
 
-  for(int shift = 0; shift < LED_COUNT; shift++) {
-
-    int modShift = shift % LED_COUNT;
-
-    for(int i=0; i < LED_COUNT; i++) {
-      int pixelHue = pixelHueStart + (i * hueInterval);
-      setPixelHue((i + modShift) % LED_COUNT, pixelHue);
-    }
-
+  for(int hue = pixelHueStart; hue < pixelHueEnd; hue += hueInterval) {
+    setRingsHue(hue);
     delay(delayBetweenUpdates);
-
   }
+
+  delay(random(1000, 20000));
+  
+  for(int hue = pixelHueEnd; hue > pixelHueStart; hue -= hueInterval) {
+    setRingsHue(hue);
+    delay(delayBetweenUpdates);
+  }
+
+  delay(random(1000, 20000));
 }
 
 void ringSetup(Adafruit_NeoPixel &ring) {
   ring.begin();
-  ring.show();            // Turn OFF all pixels ASAP
-  ring.setBrightness(15); // Set BRIGHTNESS to about 1/5 (max = 255)
+  ring.show(); // Turn OFF all pixels ASAP
+  ring.setBrightness(RINGS_BRIGHTNESS);
 }
 
-
-void setPixelHue(int i, long hue) {
-  ringRight.setPixelColor(i, ringRight.gamma32(ringRight.ColorHSV(hue)));
+void setRingsHue(long hue) {
+  for(int i=0; i < LED_COUNT; i++) {
+    ringRight.setPixelColor(i, ringRight.gamma32(ringRight.ColorHSV(hue, 255, 255)));
+    ringLeft.setPixelColor(i, ringLeft.gamma32(ringLeft.ColorHSV(hue, 255, 255)));
+  }
   ringRight.show();
-  ringLeft.setPixelColor(i, ringLeft.gamma32(ringLeft.ColorHSV(hue)));
   ringLeft.show();
 }
