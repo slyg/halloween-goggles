fa940b432c2bc8b905f73cfc46a083b2c943aa07
Sylvain Faucherand <syl.faucherand@gmail.com>
2019-10-14 15:10:35 +0100

    Rename project and cleanup indentation

--- goggles-neopixel-rings/goggles-neopixel-rings.ino
+++ goggles-neopixel-rings/goggles-neopixel-rings.ino
@@ -1,56 +1,62 @@
 #include <Adafruit_NeoPixel.h>
 
-#define LED_PIN_RIGHT      6
-#define LED_PIN_LEFT       7
-#define LED_COUNT         16  // 16 leds rings
-#define RINGS_BRIGHTNESS  50  // (0 to 255)
-#define HUE_RANGE         65536 // Number of possible hue values
+#define LED_PIN_RIGHT 6
+#define LED_PIN_LEFT 7
+#define LED_COUNT 16        // 16 leds rings
+#define RINGS_BRIGHTNESS 50 // (0 to 255)
+#define HUE_RANGE 65536     // Number of possible hue values
 
 Adafruit_NeoPixel ringRight = Adafruit_NeoPixel(LED_COUNT, LED_PIN_RIGHT, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel ringLeft = Adafruit_NeoPixel(LED_COUNT, LED_PIN_LEFT, NEO_GRB + NEO_KHZ800);
 
-void setup() {
+void setup()
+{
   ringSetup(ringRight);
   ringSetup(ringLeft);
   setRingsFadeInWithHue(0, 0, 100);
 }
 
-void loop() {
-  long pixelHueStart = 0; // Red
-  long pixelHueEnd = HUE_RANGE/6; // Orange
+void loop()
+{
+  long pixelHueStart = 0;           // Red
+  long pixelHueEnd = HUE_RANGE / 6; // Orange
   long hueInterval = 256;
   int delayBetweenUpdates = 10;
 
   setRingsFadeInWithHue(pixelHueStart, 100, 255);
 
-  for(int hue = pixelHueStart; hue < pixelHueEnd; hue += hueInterval) {
+  for (int hue = pixelHueStart; hue < pixelHueEnd; hue += hueInterval)
+  {
     setRingsHue(hue);
     delay(delayBetweenUpdates);
   }
 
   delay(random(1000, 20000));
-  
-  for(int hue = pixelHueEnd; hue > pixelHueStart; hue -= hueInterval) {
+
+  for (int hue = pixelHueEnd; hue > pixelHueStart; hue -= hueInterval)
+  {
     setRingsHue(hue);
     delay(delayBetweenUpdates);
   }
 
   delay(random(1000, 20000));
-  
+
   setRingsFadeOutWithHue(pixelHueStart, 255, 100);
 
   delay(1000);
-
 }
 
-void ringSetup(Adafruit_NeoPixel &ring) {
+void ringSetup(Adafruit_NeoPixel &ring)
+{
   ring.begin();
   ring.show(); // Turn OFF all pixels ASAP
   ring.setBrightness(RINGS_BRIGHTNESS);
 }
 
-void setRingsHue(long hue) {
-  for(int i=0; i < LED_COUNT; i++) {
+void setRingsHue(long hue)
+{
+  for (int i = 0; i < LED_COUNT; i++)
+  {
     ringRight.setPixelColor(i, ringRight.gamma32(ringRight.ColorHSV(hue, 255, 255)));
     ringLeft.setPixelColor(i, ringLeft.gamma32(ringLeft.ColorHSV(hue, 255, 255)));
   }
@@ -58,9 +64,12 @@ void setRingsHue(long hue) {
   ringLeft.show();
 }
 
-void setRingsFadeInWithHue(long hue, int minSaturation, int maxSaturation) {
-  for(int s = minSaturation; s < maxSaturation; s += 5) {
-    for(int i=0; i < LED_COUNT; i++) {
+void setRingsFadeInWithHue(long hue, int minSaturation, int maxSaturation)
+{
+  for (int s = minSaturation; s < maxSaturation; s += 5)
+  {
+    for (int i = 0; i < LED_COUNT; i++)
+    {
       ringRight.setPixelColor(i, ringRight.gamma32(ringRight.ColorHSV(hue, 255, s)));
       ringLeft.setPixelColor(i, ringLeft.gamma32(ringLeft.ColorHSV(hue, 255, s)));
     }
@@ -70,9 +79,12 @@ void setRingsFadeInWithHue(long hue, int minSaturation, int maxSaturation) {
   }
 }
 
-void setRingsFadeOutWithHue(long hue, int maxSaturation, int minSaturation) {
-  for(int s = maxSaturation; s > minSaturation; s -= 5) {
-    for(int i=0; i < LED_COUNT; i++) {
+void setRingsFadeOutWithHue(long hue, int maxSaturation, int minSaturation)
+{
+  for (int s = maxSaturation; s > minSaturation; s -= 5)
+  {
+    for (int i = 0; i < LED_COUNT; i++)
+    {
       ringRight.setPixelColor(i, ringRight.gamma32(ringRight.ColorHSV(hue, 255, s)));
       ringLeft.setPixelColor(i, ringLeft.gamma32(ringLeft.ColorHSV(hue, 255, s)));
     }
